d1777a53923d8858e49cee898ed4e8be979cdb77
Phodal Huang <h@phodal.com>
2020-02-09 14:02:03 +0800

    build: simplify build kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
-group = "com.phodal"
-version = "1.0-SNAPSHOT"
-
 plugins {
-    val kotlinVersion = "1.3.61"
+    base
 
-//    application
-    kotlin("jvm") version kotlinVersion
+    val kotlinVersion = "1.3.61"
+    kotlin("jvm") version kotlinVersion apply false
+
     java // Required by at least JUnit.
 
 //    id("org.jetbrains.kotlin.multiplatform") version "1.3.61"
@@ -13,44 +11,26 @@ plugins {
     // Test coverage
     jacoco
     // Upload jacoco coverage reports to coveralls
-     id("com.github.kt3k.coveralls") version "2.9.0"
+    id("com.github.kt3k.coveralls") version "2.9.0"
 }
 
-repositories {
-    mavenCentral()
-    mavenLocal()
-    jcenter()
+allprojects {
+    group = "com.phodal"
+    version = "1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+        mavenLocal()
+        jcenter()
+    }
 }
+
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    // To "prevent strange errors".
-    implementation(kotlin("reflect"))
-    // Kotlin reflection.
-    implementation(kotlin("test"))
-    implementation(kotlin("test-junit"))
-
-    // JUnit 5
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
-    testRuntimeOnly("org.junit.platform:junit-platform-console:1.6.0")
-
-    // Kotlintest
-    testImplementation("io.kotlintest:kotlintest-core:3.4.2")
-    testImplementation("io.kotlintest:kotlintest-assertions:3.4.2")
-    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
-
-    // JavaFX tests using TestFX
-    testImplementation("org.testfx:testfx-core:4.0.16-alpha")
-    testImplementation("org.testfx:testfx-junit:4.0.16-alpha")
-    // Only needed for headless testing.
-    // testImplementation("org.testfx:openjfx-monocle:8u76-b04") // jdk-9+181 for Java 9
-
-    // Spek
-    testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.9")
-    testImplementation("org.spekframework.spek2:spek-runner-junit5:2.0.9")
+    subprojects.forEach {
+        archives(it)
+    }
 }
-
 
 // Test coverage reporting for coveralls.
 tasks {
