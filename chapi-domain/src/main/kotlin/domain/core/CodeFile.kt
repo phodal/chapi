b6daa90510f159fef3c79938aa268379801095ab
Phodal Huang <h@phodal.com>
2020-02-10 17:00:51 +0800

    feat: <domain> add file & property to json

--- chapi-domain/src/main/kotlin/domain/core/CodeFile.kt
+++ chapi-domain/src/main/kotlin/domain/core/CodeFile.kt
@@ -1,6 +1,8 @@
 package domain.core
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
 
 @Serializable
 open class CodeFile(
@@ -11,5 +13,7 @@ open class CodeFile(
     var DataStructures: Array<CodeDataStruct> = arrayOf(),
     var Fields: Array<CodeField> = arrayOf()
 ) {
-
+    override fun toString(): String {
+        return Json(JsonConfiguration.Stable).stringify(serializer(), this)
+    }
 }
--- chapi-domain/src/main/kotlin/domain/core/CodeProperty.kt
+++ chapi-domain/src/main/kotlin/domain/core/CodeProperty.kt
@@ -1,6 +1,8 @@
 package domain.core
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
 
 @Serializable
 class CodeProperty(
@@ -11,5 +13,7 @@ class CodeProperty(
     var ReturnTypes: Array<CodeProperty> = arrayOf(),
     var Parameters: Array<CodeProperty> = arrayOf()
 ) {
-
+    override fun toString(): String {
+        return Json(JsonConfiguration.Stable).stringify(serializer(), this)
+    }
 }
