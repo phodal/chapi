701ed4a901f3e425b814146ae376cbdfb9a8d5f2
Phodal Huang <h@phodal.com>
2020-02-12 19:13:27 +0800

    test: <domain> add more tests

---
+++ chapi-domain/src/test/kotlin/domain/core/CodePackageManagerInfoTest.kt
@@ -0,0 +1,14 @@
+package domain.core
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class CodePackageManagerInfoTest {
+
+    @Test
+    fun getterSetter() {
+        val pkgInfo = CodePackageManagerInfo("project")
+        assertEquals(pkgInfo.ProjectName, "project")
+    }
+}
--- chapi-domain/src/test/kotlin/domain/core/CodeProjectTest.kt
+++ chapi-domain/src/test/kotlin/domain/core/CodeProjectTest.kt
@@ -5,5 +5,17 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 internal class CodeProjectTest {
+    @Test
+    internal fun shouldCreateProject() {
+        val pkg = CodePackage(
+            ID = "com.phodal",
+            Name = "domain"
+        )
+        val codeModule = CodeModule()
+        codeModule.Packages += pkg
+        val project = CodeProject()
+        project.Modules += codeModule
 
+        assertEquals(project.Modules.size, 1)
+    }
 }
