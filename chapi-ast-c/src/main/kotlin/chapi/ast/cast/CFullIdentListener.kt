dceb9726e06013fc49d5e8d840e9f25ce178b54f
Phodal Huang <h@phodal.com>
2020-02-12 21:46:10 +0800

    feat: <c> add data struct name support

--- chapi-ast-c/src/main/kotlin/chapi/ast/cast/CFullIdentListener.kt
+++ chapi-ast-c/src/main/kotlin/chapi/ast/cast/CFullIdentListener.kt
@@ -2,11 +2,13 @@ package chapi.ast.cast
 
 import chapi.ast.antlr.CBaseListener
 import chapi.ast.antlr.CParser
+import domain.core.CodeDataStruct
 
 import domain.core.CodeFile
 import domain.core.CodeImport
 
 open class CFullIdentListener(fileName: String) : CBaseListener() {
+    private var currentDataStruct = CodeDataStruct()
     private var codeFile: CodeFile = CodeFile(FullName = fileName)
 
     override fun enterFunctionDefinition(ctx: CParser.FunctionDefinitionContext?) {
@@ -19,6 +21,16 @@ open class CFullIdentListener(fileName: String) : CBaseListener() {
             Source = importName
         )
         codeFile.Imports += imp
+    }
+
+    override fun enterStructOrUnionSpecifier(ctx: CParser.StructOrUnionSpecifierContext?) {
+        val codeDataStruct = CodeDataStruct()
+        if (ctx!!.Identifier() != null) {
+            codeDataStruct.NodeName = ctx.Identifier().text
+        }
+
+        currentDataStruct = codeDataStruct
+        codeFile.DataStructures += codeDataStruct
     }
 
     fun getNodeInfo(): CodeFile {
--- chapi-ast-c/src/test/kotlin/chapi/ast/cast/CFullIdentListenerTest.kt
+++ chapi-ast-c/src/test/kotlin/chapi/ast/cast/CFullIdentListenerTest.kt
@@ -3,7 +3,7 @@ package chapi.ast.cast
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
-internal class CAnalyserTest {
+internal class CFullIdentListenerTest {
     @Test
     internal fun shouldGetCodeFileName() {
         val code = this::class.java.getResource("/_fixtures/examples/1-helloworld.c").readText()
@@ -24,5 +24,18 @@ internal class CAnalyserTest {
 
         assertEquals(codeFile.Imports.size, 1)
         assertEquals(codeFile.Imports[0].Source, "stdio.h")
+    }
+
+    @Test
+    internal fun shouldIdentifyStructName() {
+        val code = """
+struct list_el {
+   int val;
+};
+"""
+        val codeFile = CAnalyser().analysis(code, "helloworld.c")
+
+        assertEquals(codeFile.DataStructures.size, 1)
+        assertEquals(codeFile.DataStructures[0].NodeName, "list_el")
     }
 }
