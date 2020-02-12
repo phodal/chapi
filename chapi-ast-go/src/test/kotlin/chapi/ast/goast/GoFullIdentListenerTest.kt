318d9e1f9fc9214fd6698311d39bd111480a67b2
Phodal Huang <h@phodal.com>
2020-02-12 09:36:40 +0800

    feat: <go> add multipleline support

--- chapi-ast-go/src/test/kotlin/chapi/ast/goast/GoFullIdentListenerTest.kt
+++ chapi-ast-go/src/test/kotlin/chapi/ast/goast/GoFullIdentListenerTest.kt
@@ -26,4 +26,20 @@ import "fmt"
         assertEquals(codeFile.Imports.size, 1)
         assertEquals(codeFile.Imports[0].Source, "fmt")
     }
+
+    @Test
+    internal fun shouldIdentifyMultipleLineImport() {
+        var code = """
+package main
+
+import "fmt"
+import . "time"
+"""
+
+        val codeFile = GoAnalyser().analysis(code, "")
+        assertEquals(codeFile.Imports.size, 2)
+        assertEquals(codeFile.Imports[0].Source, "fmt")
+        assertEquals(codeFile.Imports[1].Source, "time")
+        assertEquals(codeFile.Imports[1].AsName, ".")
+    }
 }
