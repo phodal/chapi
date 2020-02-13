0e98bf31be543536ca92b2914455e45efc184c3c
Phodal Huang <h@phodal.com>
2020-02-13 14:09:08 +0800

    feat: <domain> remove unused ImportName

--- chapi-domain/src/main/kotlin/domain/core/CodeImport.kt
+++ chapi-domain/src/main/kotlin/domain/core/CodeImport.kt
@@ -6,7 +6,6 @@ import kotlinx.serialization.Serializable
 open class CodeImport(
     var Source: String = "",
     var AsName: String = "",
-    var ImportName: String = "",
     var UsageName: Array<String> = arrayOf<String>(),
     var Scope: String = ""
 ) {
