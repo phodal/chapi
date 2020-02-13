135f1bc7210224870012932b06bb499a9dcc4f85
Phodal Huang <h@phodal.com>
2020-02-13 11:22:57 +0800

    refactor: <C#> rename

--- chapi-ast-csharp/src/test/kotlin/chapi/ast/csharpast/CSharpFullIdentListenerTest.kt
+++ chapi-ast-csharp/src/test/kotlin/chapi/ast/csharpast/CSharpFullIdentListenerTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
-internal class CSharpAnalyserTest {
+internal class CSharpFullIdentListenerTest {
 
     @Test
     fun analysis() {
