7a3850e37b22b31a4e0c6c6668d2bacaf568e9cf
Phodal Huang <h@phodal.com>
2020-02-14 11:43:53 +0800

    feat: <SCALA> add object name support

--- chapi-ast-scala/src/main/kotlin/chapi/ast/scalaast/ScalaFullIdentListener.kt
+++ chapi-ast-scala/src/main/kotlin/chapi/ast/scalaast/ScalaFullIdentListener.kt
@@ -1,10 +1,21 @@
 package chapi.ast.scalaast
 
+import chapi.ast.antlr.ScalaParser
 import chapi.domain.core.CodeContainer
+import chapi.domain.core.CodeDataStruct
 
 class ScalaFullIdentListener(var fileName: String) : ScalaAstBaseListener() {
-    private var codeContainer: CodeContainer =
-        CodeContainer(FullName = fileName)
+    private var codeContainer: CodeContainer = CodeContainer(FullName = fileName)
+
+    override fun enterObjectDef(ctx: ScalaParser.ObjectDefContext?) {
+        val objectName = ctx!!.Id().text
+        val codeDataStruct = CodeDataStruct(
+            NodeName = objectName,
+            FilePath = codeContainer.FullName
+        )
+
+        codeContainer.DataStructures += codeDataStruct
+    }
 
     fun getNodeInfo(): CodeContainer {
         return this.codeContainer
--- chapi-ast-scala/src/test/kotlin/chapi/ast/scalaast/ScalaFullIdentListenerTest.kt
+++ chapi-ast-scala/src/test/kotlin/chapi/ast/scalaast/ScalaFullIdentListenerTest.kt
@@ -1,6 +1,7 @@
 package chapi.ast.scalaast
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 internal class ScalaFullIdentListenerTest {
     private val hellowoorld: String = """
@@ -14,5 +15,12 @@ object HelloWorld {
     @Test
     internal fun shouldAnalysis() {
         ScalaAnalyser().analysis(hellowoorld, "hello.scala")
+    }
+
+    @Test
+    internal fun shouldAnalysisObjectName() {
+        val container = ScalaAnalyser().analysis(hellowoorld, "hello.scala")
+        assertEquals(container.DataStructures.size, 1)
+        assertEquals(container.DataStructures[0].NodeName, "HelloWorld")
     }
 }
