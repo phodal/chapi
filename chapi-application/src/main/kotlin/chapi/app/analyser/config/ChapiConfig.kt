853fe590a2e6d1bb5f327ddda2e0ebbcfbfdff24
Phodal Huang <h@phodal.com>
2020-02-21 09:17:57 +0800

    feat: add filter rule for chapi config

--- chapi-application/src/main/kotlin/chapi/app/analyser/config/ChapiConfig.kt
+++ chapi-application/src/main/kotlin/chapi/app/analyser/config/ChapiConfig.kt
@@ -2,7 +2,8 @@ package chapi.app.analyser.config
 
 data class ChapiConfig(
     var language: String = "",
-    var withoutTest: Boolean = false
+    var withoutTest: Boolean = false,
+    var filterRule: String = ""
 ) {
 
 }
--- chapi-application/src/main/kotlin/chapi/app/analyser/filter/FileFilter.kt
+++ chapi-application/src/main/kotlin/chapi/app/analyser/filter/FileFilter.kt
@@ -5,6 +5,10 @@ import chapi.app.analyser.config.ChapiConfig
 open class FileFilter {
     companion object {
         fun filterByLanguage(path: String, config: ChapiConfig): Boolean {
+            if (config.filterRule.isNotEmpty()) {
+                return config.filterRule.toRegex().matches(path)
+            }
+
             return when (config.language) {
                 "java" -> {
                     path.endsWith(".java")
