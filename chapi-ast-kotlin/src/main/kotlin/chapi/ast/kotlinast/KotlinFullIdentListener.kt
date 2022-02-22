c1f32da38cdf3f2dabb250d7ec0de53c59971b09
Phodal Huang <h@phodal.com>
2022-02-22 13:22:03 +0800

    feat(kotlin): add ident for classname

--- chapi-ast-kotlin/src/main/kotlin/chapi/ast/kotlinast/KotlinFullIdentListener.kt
+++ chapi-ast-kotlin/src/main/kotlin/chapi/ast/kotlinast/KotlinFullIdentListener.kt
@@ -2,15 +2,41 @@ package chapi.ast.kotlinast
 
 import chapi.ast.antlr.KotlinParser
 import chapi.domain.core.CodeContainer
+import chapi.domain.core.CodeDataStruct
+import chapi.domain.core.DataStructType
 
 class KotlinFullIdentListener(fileName: String) : KotlinAstListener() {
+    private var classNodes: Array<CodeDataStruct> = emptyArray()
+    private lateinit var currentType: DataStructType
+    private var currentClz: String = "";
+    private var hasEnterClass: Boolean = false
+    private var currentNode = CodeDataStruct()
     private var codeContainer: CodeContainer = CodeContainer(FullName = fileName)
 
     override fun enterFunctionDeclaration(ctx: KotlinParser.FunctionDeclarationContext?) {
         super.enterFunctionDeclaration(ctx)
     }
 
+    override fun enterClassDeclaration(ctx: KotlinParser.ClassDeclarationContext?) {
+        currentType = DataStructType.CLASS
+
+        hasEnterClass = true
+
+        currentNode.Package = codeContainer.PackageName
+        currentNode.FilePath = codeContainer.FullName
+
+        if (ctx!!.simpleIdentifier().Identifier() != null) {
+            currentClz = ctx.simpleIdentifier().Identifier().text
+            currentNode.NodeName = currentClz
+        }
+    }
+
+    override fun exitClassDeclaration(ctx: KotlinParser.ClassDeclarationContext?) {
+        classNodes += currentNode
+    }
+
     fun getNodeInfo(): CodeContainer {
+        codeContainer.DataStructures = classNodes
         return codeContainer
     }
 }
--- chapi-ast-kotlin/src/test/kotlin/chapi/ast/kotlinast/KotlinAnalyserTest.kt
+++ chapi-ast-kotlin/src/test/kotlin/chapi/ast/kotlinast/KotlinAnalyserTest.kt
@@ -15,4 +15,19 @@ fun main() {
         val codeContainer = KotlinAnalyser().analysis(code, "hello.kt")
         assertEquals(codeContainer.FullName, "hello.kt")
     }
+
+    @Test
+    internal fun shouldIdentifyKotlinClass() {
+        val code = """
+class Person(val name: String) {
+    val children: MutableList<Person> = mutableListOf()
+    constructor(name: String, parent: Person) : this(name) {
+        parent.children.add(this)
+    }
+}
+"""
+
+        val codeContainer = KotlinAnalyser().analysis(code, "hello.kt")
+        assertEquals(codeContainer.DataStructures[0].NodeName, "Person")
+    }
 }
