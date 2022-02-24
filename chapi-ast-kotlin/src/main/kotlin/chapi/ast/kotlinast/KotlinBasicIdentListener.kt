ec5d0563d9143c963c76eb01f269ec11d0c8ccc4
Andong Liao <adliao@thoughtworks.com>
2022-02-24 21:09:34 +0800

    refactor: re-pass the test cases

--- chapi-ast-kotlin/src/main/kotlin/chapi/ast/kotlinast/KotlinBasicIdentListener.kt
+++ chapi-ast-kotlin/src/main/kotlin/chapi/ast/kotlinast/KotlinBasicIdentListener.kt
@@ -2,6 +2,8 @@ package chapi.ast.kotlinast
 
 import chapi.ast.antlr.KotlinParser
 import chapi.domain.core.CodeContainer
+import chapi.domain.core.CodeDataStruct
+import chapi.domain.core.DataStructType
 
 /**
  * basic identifier listener, include
@@ -14,14 +16,35 @@ class KotlinBasicIdentListener(fileName: String) : KotlinAstListener() {
     /** inner storage */
 
     private val codeContainer: CodeContainer = CodeContainer(FullName = fileName)
+    private val classNodes: MutableList<CodeDataStruct> = mutableListOf()
+    private lateinit var currentNode: CodeDataStruct
 
     /** outer interfaces */
 
-    override fun getNodeInfo(): CodeContainer = codeContainer
+    override fun getNodeInfo(): CodeContainer = codeContainer.apply {
+        DataStructures = classNodes.toTypedArray()
+    }
 
     /** override hooks */
 
-    override fun enterPackageHeader(ctx: KotlinParser.PackageHeaderContext?) {
-        super.enterPackageHeader(ctx)
+    override fun enterPackageHeader(ctx: KotlinParser.PackageHeaderContext) {
+        if (ctx.childCount == 0) return
+
+        codeContainer.PackageName = ctx.identifier().text
+    }
+
+    override fun enterClassDeclaration(ctx: KotlinParser.ClassDeclarationContext) {
+        currentNode = CodeDataStruct()
+        currentNode.Type = DataStructType.CLASS
+        currentNode.Package = codeContainer.PackageName
+        currentNode.NodeName = ctx.simpleIdentifier().Identifier().text
+    }
+
+    override fun exitClassDeclaration(ctx: KotlinParser.ClassDeclarationContext?) {
+        classNodes.add(currentNode)
+    }
+
+    companion object {
+        const val UNKNOWN_PLACEHOLDER = "<UNKNOWN>"
     }
 }
