f9d42e2342a942d70095e0b138ca7d15d3a63726
Andong Liao <adliao@thoughtworks.com>
2022-02-25 21:37:43 +0800

    feat: parse annotation (only work for single)

--- chapi-ast-kotlin/src/main/kotlin/chapi/ast/kotlinast/KotlinBasicIdentListener.kt
+++ chapi-ast-kotlin/src/main/kotlin/chapi/ast/kotlinast/KotlinBasicIdentListener.kt
@@ -1,11 +1,14 @@
 package chapi.ast.kotlinast
 
 import chapi.ast.antlr.KotlinParser
+import chapi.domain.core.AnnotationKeyValue
+import chapi.domain.core.CodeAnnotation
 import chapi.domain.core.CodeContainer
 import chapi.domain.core.CodeDataStruct
 import chapi.domain.core.CodeFunction
 import chapi.domain.core.CodeImport
 import chapi.domain.core.CodePosition
+import chapi.domain.core.CodeProperty
 import chapi.domain.core.DataStructType
 import org.antlr.v4.runtime.ParserRuleContext
 
@@ -45,14 +48,15 @@ class KotlinBasicIdentListener(fileName: String) : KotlinAstListener() {
     }
 
     override fun enterClassDeclaration(ctx: KotlinParser.ClassDeclarationContext) {
+        val implements = ctx.delegationSpecifiers()?.text?.let(::getTypeFullName)?.let(::listOf) ?: emptyList()
+        val annotations = ctx.modifiers().getAnnotations()
+
         currentNode = CodeDataStruct()
         currentNode.Type = DataStructType.CLASS
         currentNode.Package = codeContainer.PackageName
         currentNode.NodeName = ctx.simpleIdentifier().Identifier().text
-
-        ctx.delegationSpecifiers()?.let {
-            currentNode.Implements = arrayOf(it.text.let(::getTypeFullName))
-        }
+        currentNode.Implements = implements.toTypedArray()
+        currentNode.Annotations = annotations.toTypedArray()
     }
 
     override fun exitClassDeclaration(ctx: KotlinParser.ClassDeclarationContext) {
@@ -60,13 +64,18 @@ class KotlinBasicIdentListener(fileName: String) : KotlinAstListener() {
     }
 
     override fun enterPrimaryConstructor(ctx: KotlinParser.PrimaryConstructorContext) {
+        val parameters = ctx.classParameters().classParameter().map(::buildProperty)
+        val annotations = ctx.modifiers().getAnnotations()
+
         currentFunction = CodeFunction(
             Name = PRIMARY_CONSTRUCTOR_NAME,
             ReturnType = currentNode.getClassFullName(),
             Override = false,
             Position = ctx.getPosition(),
             IsConstructor = true,
-            Package = codeContainer.PackageName
+            Package = codeContainer.PackageName,
+            Parameters = parameters.toTypedArray(),
+            Annotations = annotations.toTypedArray()
         )
     }
 
@@ -75,26 +84,62 @@ class KotlinBasicIdentListener(fileName: String) : KotlinAstListener() {
     }
 
     override fun enterImportList(ctx: KotlinParser.ImportListContext) {
-        ctx.importHeader()
-            .map(KotlinParser.ImportHeaderContext::identifier)
-            .map { CodeImport(Source = it.text, AsName = it.text.substringAfterLast('.')) }
-            .let(imports::addAll)
+        fun buildImport(header: KotlinParser.ImportHeaderContext) = header.identifier().run {
+            CodeImport(Source = text, AsName = text.substringAfterLast('.'))
+        }
+
+        ctx.importHeader().map(::buildImport).let(imports::addAll)
     }
 
     override fun enterFunctionDeclaration(ctx: KotlinParser.FunctionDeclarationContext) {
+        val parameters = ctx.functionValueParameters().functionValueParameter()
+            .map(KotlinParser.FunctionValueParameterContext::parameter)
+            .map(::buildProperty)
+        val annotations = ctx.modifiers().getAnnotations()
+
         currentFunction = CodeFunction(
             Name = ctx.simpleIdentifier()?.Identifier()?.text ?: UNKNOWN_PLACEHOLDER,
             ReturnType = getTypeFullName(ctx.type()?.typeReference()?.text ?: UNIT_TYPE),
             Override = false,
             Position = ctx.getPosition(),
             IsConstructor = false,
-            Package = codeContainer.PackageName
+            Package = codeContainer.PackageName,
+            Parameters = parameters.toTypedArray(),
+            Annotations = annotations.toTypedArray()
         )
     }
+
+    private fun buildProperty(it: KotlinParser.ClassParameterContext) =
+        CodeProperty(TypeValue = it.simpleIdentifier().text, TypeType = getTypeFullName(it.type().text))
+
+    private fun buildProperty(it: KotlinParser.ParameterContext) =
+        CodeProperty(TypeValue = it.simpleIdentifier().text, TypeType = getTypeFullName(it.type().text))
 
     override fun exitFunctionDeclaration(ctx: KotlinParser.FunctionDeclarationContext?) {
         currentNode.Functions += currentFunction
     }
+
+    private fun KotlinParser.ModifiersContext?.getAnnotations(): List<CodeAnnotation> =
+        this?.annotation()?.map(::buildAnnotation) ?: emptyList()
+
+    private fun buildAnnotation(it: KotlinParser.AnnotationContext): CodeAnnotation =
+        CodeAnnotation(
+            Name = it.singleAnnotation()
+                .unescapedAnnotation()
+                .constructorInvocation()
+¡                .userType()
+                .simpleUserType().first()
+                .simpleIdentifier().Identifier().text,
+            KeyValues = it.singleAnnotation()
+                .unescapedAnnotation()
+                .constructorInvocation()
+                .valueArguments().valueArgument().map {
+                    AnnotationKeyValue(
+                        Key = it.simpleIdentifier().text,
+                        Value = it.expression().text,
+                    )
+                }.toTypedArray()
+        )
 
     private fun ParserRuleContext.getPosition(): CodePosition =
         CodePosition(
