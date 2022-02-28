c0d5d9b98e6279db5a24524350e99f2b31289bee
Phodal Huang <h@phodal.com>
2022-02-28 19:35:07 +0800

    build: upgrade jacoco plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,18 +8,17 @@ plugins {
     id("maven-publish")
     publishing
     signing
-//    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 
     java
     jacoco
+    id("jacoco-report-aggregation")
     id("com.github.kt3k.coveralls") version "2.9.0"
-
-    // todo: erich domain testing & logic
-    id("nl.fabianm.kotlin.plugin.generated") version "1.5.0" // hack for jacoco generate code coverage
 }
 
+
 jacoco {
-    toolVersion = "0.8.5"
+    toolVersion = "0.8.7"
+    reportsDirectory.set(layout.buildDirectory.dir("customJacocoReportDir"))
 }
 
 allprojects {
@@ -39,6 +38,7 @@ subprojects {
     apply(plugin = "maven-publish")
     apply(plugin = "signing")
     apply(plugin = "publishing")
+    apply(plugin = "jacoco")
 
     publishing {
         publications {
@@ -108,82 +108,22 @@ subprojects {
         withJavadocJar()
         withSourcesJar()
     }
-}
 
-val jacocoReports by configurations.creating {
-    isCanBeResolved = true
-    isCanBeConsumed = false
-    attributes {
-        attribute(Usage.USAGE_ATTRIBUTE, project.objects.named(Usage::class, "jacocoReports"))
-    }
-}
 
-dependencies {
-    jacocoReports(project(":chapi-domain"))
-    jacocoReports(project(":chapi-application"))
 
-    jacocoReports(project(":chapi-ast-java"))
-    jacocoReports(project(":chapi-ast-typescript"))
-    jacocoReports(project(":chapi-ast-python"))
-    jacocoReports(project(":chapi-ast-go"))
-    jacocoReports(project(":chapi-ast-kotlin"))
-    jacocoReports(project(":chapi-ast-c"))
-    jacocoReports(project(":chapi-ast-csharp"))
-    jacocoReports(project(":chapi-ast-scala"))
-    jacocoReports(project(":chapi-ast-cpp"))
-
-    subprojects.forEach {
-        archives(it)
-    }
-}
-
-// refs: https://github.com/ben-manes/caffeine/blob/v2.6.2/build.gradle#L133
-val jacocoMerge by tasks.registering(JacocoMerge::class) {
-    subprojects.forEach { subproject ->
-        executionData(subproject.tasks.withType(Test::class.java))
-    }
-    doFirst {
-        executionData = files(executionData.filter {
-            it.exists()
-        })
-    }
-}
-
-// refs: https://github.com/stankevichevg/axon-couchbase/blob/b455cbf420963656adf8f3d964c68f9811c9e8e3/build.gradle.kts
-tasks.register("jacocoRootReports", JacocoReport::class) {
-    dependsOn("test")
-    dependsOn(jacocoMerge)
-
-    executionData(files(jacocoMerge))
-    additionalClassDirs(files(subprojects.flatMap { project ->
-        listOf("kotlin")
-            .map {
-                project.buildDir.path + "/classes/$it/main"
-            }
-            .map {
-                fileTree(it).apply {
-                    exclude("chapi/ast/antlr", "chapi/domain")
-                }
-            }
-    }))
-    additionalSourceDirs(files(subprojects.flatMap { project ->
-        listOf("kotlin")
-            .map {
-                project.file("src/main/$it").absolutePath
-            }
-            .map {
-                fileTree(it).apply {
-                    exclude("chapi/ast/antlr", "chapi/domain")
-                }
-            }
-    }))
-
-    reports {
-        xml.required.set(false)
-        csv.required.set(false)
-        html.required.set(true)
-        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    tasks.test {
+        finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
     }
 
-    dependsOn(jacocoMerge)
+    tasks.jacocoTestReport {
+        dependsOn(tasks.test) // tests are required to run before generating the report
+    }
+
+    tasks.jacocoTestReport {
+        reports {
+            xml.required.set(false)
+            csv.required.set(false)
+            html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+        }
+    }
 }
