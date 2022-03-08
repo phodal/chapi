6d46120b5c56dd94686831667e5c68238e4ea06c
Phodal Huang <h@phodal.com>
2022-03-08 18:52:52 +0800

    feat: add path count

--- chapi-application/src/main/kotlin/chapi/app/path/EcmaImportPath.kt
+++ chapi-application/src/main/kotlin/chapi/app/path/EcmaImportPath.kt
@@ -7,6 +7,12 @@ import java.io.File
 //
 // output: to normalize path
 fun ecmaPathConvert(basedPath: String, sourcePath: String): String {
+    // import "@/src/component/Hello.js"
+    val isResolvePath = sourcePath.startsWith("@/")
+    if(isResolvePath) {
+        return sourcePath.removeRange(0, 2)
+    }
+
     var file = File(basedPath)
 
     // use parent to convert
@@ -16,4 +22,14 @@ fun ecmaPathConvert(basedPath: String, sourcePath: String): String {
 
     val resolve = file.resolve(File(sourcePath))
     return resolve.normalize().toString()
+}
+
+fun relativeRoot(basedPath: String, sourcePath: String): String {
+    var pathname = sourcePath
+    val isResolvePath = pathname.startsWith("@/")
+    if(isResolvePath) {
+        pathname = pathname.removeRange(0, 2)
+    }
+
+    return File(pathname).relativeTo(File(basedPath)).toString()
 }
--- chapi-application/src/test/kotlin/chapi/app/path/EcmaImportPathKtTest.kt
+++ chapi-application/src/test/kotlin/chapi/app/path/EcmaImportPathKtTest.kt
@@ -3,7 +3,7 @@ package chapi.app.path
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-internal class EcmaPathConvertKtTest {
+internal class EcmaImportPathKtTest {
 
     @Test
     internal fun sameLevelPath() {
@@ -21,5 +21,25 @@ internal class EcmaPathConvertKtTest {
     internal fun fileSameLevel() {
         val path = ecmaPathConvert("src/components/Hello.js", "../config.js")
         assertEquals(path, "src/components/config.js")
+    }
+
+    @Test
+    internal fun resolvePath() {
+        val path = ecmaPathConvert("src/components/Hello.js", "@/src/components/config.js")
+        assertEquals(path, "src/components/config.js")
+    }
+
+    @Test
+    internal fun resolveRealWorldPath() {
+        val full = "chapi/chapi-application/ts/apicall/addition/"
+        val path = ecmaPathConvert(full, "../components/config.js")
+        assertEquals(path, "chapi/chapi-application/ts/apicall/components/config.js")
+    }
+
+    // source: /Volumes/source/modernizing/chapi/chapi-application/build/resources/test/languages/ts/apicall/addition/systemInfo.ts
+    @Test
+    internal fun relativePath() {
+        val path = relativeRoot("root/src/components/Hello.js", "root/src/components/config.js")
+        assertEquals(path, "../config.js")
     }
 }
