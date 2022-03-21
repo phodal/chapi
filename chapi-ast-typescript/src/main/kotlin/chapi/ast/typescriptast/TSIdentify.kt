13f3b0c03fd788b8c71f6da7908765161c8dcf8b
Phodal Huang <h@phodal.com>
2022-03-21 10:15:41 +0800

    feat(ts): add basic parser for resolve path

--- chapi-ast-typescript/src/main/kotlin/chapi/ast/typescriptast/TSIdentify.kt
+++ chapi-ast-typescript/src/main/kotlin/chapi/ast/typescriptast/TSIdentify.kt
@@ -3,5 +3,19 @@ package chapi.ast.typescriptast
 open class TSIdentify(
     var filePath: String = ""
 ) {
+    fun resolvePackage(): String {
+        val split = filePath.split("/").toMutableList()
+        if(split.size == 1) {
+            return split[0]
+        }
 
+        if(split[0] == "src" || split[0] == "") {
+            split[0] = "@"
+        } else {
+            split.add(0, "@")
+        }
+
+        val names = split.dropLast(1)
+        return names.joinToString(".")
+    }
 }
---
+++ chapi-ast-typescript/src/test/kotlin/chapi/ast/typescriptast/TSIdentifyTest.kt
@@ -0,0 +1,47 @@
+package chapi.ast.typescriptast
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class TSIdentifyTest {
+
+    @Test
+    internal fun `return origin file name when is one File `() {
+        val tsIdentify = TSIdentify("AbstractClass.ts")
+        val pkgName = tsIdentify.resolvePackage()
+
+        assertEquals("AbstractClass.ts", pkgName)
+    }
+
+    @Test
+    internal fun `return to start with @ with slash start file `() {
+        val tsIdentify = TSIdentify("/AbstractClass.ts")
+        val pkgName = tsIdentify.resolvePackage()
+
+        assertEquals("@", pkgName)
+    }
+
+    @Test
+    internal fun `return to start with src with slash start file `() {
+        val tsIdentify = TSIdentify("src/AbstractClass.ts")
+        val pkgName = tsIdentify.resolvePackage()
+
+        assertEquals("@", pkgName)
+    }
+
+    @Test
+    internal fun `return to start with src with name`() {
+        val tsIdentify = TSIdentify("src/grammar/AbstractClass.ts")
+        val pkgName = tsIdentify.resolvePackage()
+
+        assertEquals("@.grammar", pkgName)
+    }
+
+    @Test
+    internal fun `add @ for start with slash`() {
+        val tsIdentify = TSIdentify("/grammar/AbstractClass.ts")
+        val pkgName = tsIdentify.resolvePackage()
+
+        assertEquals("@.grammar", pkgName)
+    }
+}
