645567c3e4d58d5051bed670e2fcc2b3198bc438
Phodal Huang <h@phodal.com>
2022-03-21 18:29:25 +0800

    chore: release 1.2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ jacoco {
 
 allprojects {
     group = "com.phodal.chapi"
-    version = "1.2.2"
+    version = "1.2.3"
     description =
         "Chapi is A common language meta information convertor, convert different languages to same meta-data model"
 
