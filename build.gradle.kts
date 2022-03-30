9bc6979e1f0b7c98fa68c1358e3157a332cc62d8
Phodal Huang <h@phodal.com>
2022-03-30 10:18:13 +0800

    build: version to snapshot for local test

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ allprojects {
     apply(plugin = "java")
 
     group = "com.phodal.chapi"
-    version = "1.3.1"
+    version = "1.4.0-SNAPSHOT"
     description =
         "Chapi is A common language meta information convertor, convert different languages to same meta-data model"
 
