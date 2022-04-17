9c76aaa496f1aff6701d26a7f49377f99c8f6723
Phodal Huang <h@phodal.com>
2022-04-17 21:08:20 +0800

    chore: release 1.5.7 for #15 and archguard/archguard#30

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ allprojects {
     apply(plugin = "java")
 
     group = "com.phodal.chapi"
-    version = "1.5.6"
+    version = "1.5.7"
     description =
         "Chapi is A common language meta information convertor, convert different languages to same meta-data model"
 
