fcb1a20230a1e5f8d19fdb4ba2d3a9da5fe6480b
Phodal Huang <h@phodal.com>
2022-11-18 09:30:44 +0800

    chore: release 2.0.0-alpha.7 for typescript

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ allprojects {
     apply(plugin = "java")
 
     group = "com.phodal.chapi"
-    version = "2.0.0-beta.6"
+    version = "2.0.0-beta.7"
     description =
         "Chapi is A common language meta information convertor, convert different languages to same meta-data model"
 
