1e38b755680cf388fc7642d546755a72126b6524
Phodal Huang <h@phodal.com>
2023-02-16 10:56:23 +0800

    chore: release 2.1.0 for dataframe

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ allprojects {
     apply(plugin = "java")
 
     group = "com.phodal.chapi"
-    version = "2.0.0"
+    version = "2.1.0"
     description =
         "Chapi is A common language meta information convertor, convert different languages to same meta-data model"
 
