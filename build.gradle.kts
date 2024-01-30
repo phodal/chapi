2109c7faacd65fd5628f9852d4cb2995d0f08cf7
Phodal Huang <h@phodal.com>
2024-01-30 13:42:01 +0800

    chore: release to 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ allprojects {
     apply(plugin = "java")
 
     group = "com.phodal.chapi"
-    version = "2.2.5"
+    version = "2.3.0"
     description =
         "Chapi is A common hierarchical abstract parser && information convertor, streamlines code analysis by converting diverse language source code into a unified abstract model, simplifying cross-language development."
 
