aabd390072118a06f520801afd8c1154d70223e3
Phodal Huang <h@phodal.com>
2024-02-01 11:36:35 +0800

    feat(c): add support for C# preprocessor directives from https://gist.github.com/KvanTTT/d95579de257531a3cc15
    This commit adds support for C# preprocessor directives in the CSharpAnalyser class. It introduces a new import statement for the CSharpPreprocessorParser and modifies the parse() function to handle preprocessor directives. The function now collects preprocessor directive tokens, parses the condition in the directive, and determines whether the following code should be compiled or ignored. This enhancement allows the analyser to correctly handle C# code with preprocessor directives.

--- chapi-ast-csharp/src/main/kotlin/chapi/ast/csharpast/CSharpAnalyser.kt
+++ chapi-ast-csharp/src/main/kotlin/chapi/ast/csharpast/CSharpAnalyser.kt
@@ -2,10 +2,10 @@ package chapi.ast.csharpast
 
 import chapi.ast.antlr.CSharpLexer
 import chapi.ast.antlr.CSharpParser
+import chapi.ast.antlr.CSharpPreprocessorParser
 import chapi.domain.core.CodeContainer
 import chapi.parser.Analyser
-import org.antlr.v4.runtime.CharStreams
-import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.*
 import org.antlr.v4.runtime.tree.ParseTreeWalker
 
 open class CSharpAnalyser : Analyser {
@@ -18,9 +18,64 @@ open class CSharpAnalyser : Analyser {
         return listener.getNodeInfo()
     }
 
-    private fun parse(str: String): CSharpParser =
-        CharStreams.fromString(str)
-            .let(::CSharpLexer)
-            .let(::CommonTokenStream)
-            .let(::CSharpParser)
+    private fun parse(str: String): CSharpParser {
+        val codeTokens: MutableList<Token> = mutableListOf()
+        val commentTokens: MutableList<Token> = mutableListOf()
+
+        val preprocessorLexer = CSharpLexer(CharStreams.fromString(str))
+        val tokens = preprocessorLexer.allTokens
+        val directiveTokens: MutableList<Token> = mutableListOf()
+        var directiveTokenSource = ListTokenSource(directiveTokens)
+        var directiveTokenStream = CommonTokenStream(directiveTokenSource, CSharpLexer.DIRECTIVE)
+        val preprocessorParser = CSharpPreprocessorParser(directiveTokenStream)
+
+        var index = 0
+        var compiledTokens = true
+        while (index < tokens.size) {
+            val token = tokens[index]
+            when {
+                token.type == CSharpLexer.SHARP -> {
+                    directiveTokens.clear()
+                    var directiveTokenIndex = index + 1
+                    // Collect all preprocessor directive tokens.
+                    while (directiveTokenIndex < tokens.size &&
+                        tokens[directiveTokenIndex].type != CSharpLexer.EOF &&
+                        tokens[directiveTokenIndex].type != CSharpLexer.DIRECTIVE_NEW_LINE &&
+                        tokens[directiveTokenIndex].type != CSharpLexer.SHARP
+                    ) {
+                        if (tokens[directiveTokenIndex].channel == CSharpLexer.COMMENTS_CHANNEL) {
+                            commentTokens.add(tokens[directiveTokenIndex])
+                        } else if (tokens[directiveTokenIndex].channel != Lexer.HIDDEN) {
+                            directiveTokens.add(tokens[directiveTokenIndex])
+                        }
+                        directiveTokenIndex++
+                    }
+
+                    directiveTokenSource = ListTokenSource(directiveTokens)
+                    directiveTokenStream = CommonTokenStream(directiveTokenSource, CSharpLexer.DIRECTIVE)
+                    preprocessorParser.inputStream = directiveTokenStream
+                    preprocessorParser.reset()
+                    // Parse condition in preprocessor directive (based on CSharpPreprocessorParser.g4 grammar).
+                    val directive: CSharpPreprocessorParser.Preprocessor_directiveContext = preprocessorParser.preprocessor_directive()
+                    // if true than next code is valid and not ignored.
+                    compiledTokens = directive.value
+                    index = directiveTokenIndex - 1
+                }
+                token.channel == CSharpLexer.COMMENTS_CHANNEL -> {
+                    commentTokens.add(token) // Collect comment tokens (if required).
+                }
+                token.channel != Lexer.HIDDEN && token.type != CSharpLexer.DIRECTIVE_NEW_LINE && compiledTokens -> {
+                    codeTokens.add(token) // Collect code tokens.
+                }
+            }
+
+            index++
+        }
+
+        // At the second stage, tokens are parsed in the usual way.
+        val codeTokenSource = ListTokenSource(tokens)
+        val codeTokenStream = CommonTokenStream(codeTokenSource)
+        val parser = CSharpParser(codeTokenStream)
+        return parser
+    }
 }
